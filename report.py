b8603aa068032651fec665de15059b7e64d56cc8
Marvin Carpenter <marvin.carpenter@gmail.com>
2023-01-21 14:22:57 -0600

    corrected params

--- report.py
+++ report.py
@@ -1,12 +1,12 @@
 import sys, argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("t","--team-map",help="Path to the team map file", type=str)
-parser.add_argument("p","--product-master",help="Path to the product master file",type=str)
-parser.add_argument("s","--sales",help="Path to the sales file",type=str)
-parser.add_argument("tr","--team-report",help="Path to the team report",type=str)
-parser.add_argument("pr","--product-report",help="Path to the product report",type=str)
-parser.add_argument("h","--help",help="Display help",type=str)
+parser.add_argument("-t","--team-map",help="Path to the team map file", type=str)
+parser.add_argument("-p","--product-master",help="Path to the product master file",type=str)
+parser.add_argument("-s","--sales",help="Path to the sales file",type=str)
+parser.add_argument("-tr","--team-report",help="Path to the team report",type=str)
+parser.add_argument("-pr","--product-report",help="Path to the product report",type=str)
+parser.add_argument("-h","--help",help="Display help",type=str)
 
 args = parser.parse_args()
 
